fe328844367609b319587a42660049bfba485956
clad_men <b.ef.en.h.a.ren@googlemail.com>
2023-08-17 21:17:03 +0400

    some shit taki happened

--- about/js/background.js
+++ about/js/background.js
@@ -45,7 +45,7 @@ $(document).ready(() => {
     }
     else{
         let canvas= document.getElementById("imagec").getContext("2d")
-        canvas.fillStyle ="#040289"
+        canvas.fillStyle ="rgb(4,2,137)"
         ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     }
